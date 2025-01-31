c5cc5516ea489f9ab0f70eef93585068f749e9f7
Andre Sionek <andresionek91@gmail.com>
2025-01-31 15:38:37 +0000

    chore: test deploy actions

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from catena_cdk.environments import UsEast1Environments as Envs
 app = cdk.App()
 
 # Create an empty stack to validate the deploy action
-cdk.Stack(scope=app, id="Actions-Development", env=Envs.development)
+cdk.Stack(
+    scope=app,
+    id="Actions-Development",
+    env=Envs.development,
+)
 
 app.synth()
